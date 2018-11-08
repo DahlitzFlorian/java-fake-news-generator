6e183e9c6eddf318b7b709b8c77ea005f926f437
florian dahlitz <f2dahlitz@freenet.de>
2018-11-08 21:47:57 +0100

    feat: added crawler basic structure

---
+++ src/main/java/TextAllocation/Crawler.java
@@ -0,0 +1,20 @@
+package TextAllocation;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Crawler {
+
+    // Give it a URL and it makes an HTTP request for a web page
+    public void crawl(String nextURL) { return; }
+
+    // Tries to find a word on the page
+    public boolean searchForWord(String word) {
+        return true;
+    }
+
+    // Returns a list of all the URLs on the page
+    public List<String> getLinks() {
+        return new LinkedList<String>();
+    }
+}
