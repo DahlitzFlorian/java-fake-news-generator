984ac55108d36ace8ec7b0390243bcba9f8fe892
Jan-Niklas FICHTE <s170657@student.dhbw-mannheim.de>
2018-11-11 19:22:15 +0100

    Refactoring for english wording. Classes renamed to RSSFeed*

---
+++ src/main/java/Newscollection/RSSFeedParserInterface.java
@@ -0,0 +1,7 @@
+import org.json.JSONObject;
+
+public interface RSSFeedParserInterface {
+    public JSONObject articleCollector(String keyword);
+    public String articleCutter(String article);
+    public JSONObject articleJSONBuilder(JSONObject articleJson, String News, String title);
+}
---
+++ src/main/java/Newscollection/RSSFeedParserMain.java
@@ -0,0 +1,7 @@
+public class RSSFeedParserMain {
+    public static void main(String[] args) {
+        String key = "Jahr";
+      RSSFeedParser Test = new RSSFeedParser();
+       Test.articleCollector(key);
+    }
+}
