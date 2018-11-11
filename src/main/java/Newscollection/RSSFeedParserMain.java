940ab095ace735fe809749150eadc2a61a4280e2
Jan-Niklas FICHTE <s170657@student.dhbw-mannheim.de>
2018-11-11 19:34:06 +0100

    Only Interface to fix master

--- src/main/java/Newscollection/RSSFeedParserMain.java
+++
@@ -1,7 +0,0 @@
-public class RSSFeedParserMain {
-    public static void main(String[] args) {
-        String key = "Jahr";
-      RSSFeedParser Test = new RSSFeedParser();
-       Test.articleCollector(key);
-    }
-}
