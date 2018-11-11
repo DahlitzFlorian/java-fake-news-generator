12741f0f72ec25cc60647667f552a78d60e20ca1
Tim Leuschner <tim.leuschner@gmail.com>
2018-11-11 20:06:58 +0100

    Added the logic behind the GUI

--- src/main/java/Gui/ControllerConfiguration.java
+++ src/main/java/Gui/ControllerConfiguration.java
@@ -1,6 +1,100 @@
 package Gui;
 
+import Exceptions.InputFieldEmptyException;
+import Exceptions.InvalidInputException;
+import Utils.GuiParser;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import Utils.Popups;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+/**
+ * @author Tim Leuschner
+ */
+
 public class ControllerConfiguration {
+
+    private static final Logger log = Logger.getLogger(ControllerMainWindow.class.getName());
+
+    @FXML
+    public Button btnSaveConfig;
+
+    @FXML
+    public TextArea txtAreaNewsSources;
+
+    @FXML
+    public TextField txtFieldMaxWordCount;
+
+    @FXML
+    public TextField txtFieldMinWordCount;
+
+
+    /**
+     * Saves the parameters provided by the user into
+     */
+    public void saveConfig() {
+        JsonObject configJson = generateJson();
+        if (configJson != null) {
+            try (FileWriter file = new FileWriter("config.json")) {
+                file.write(configJson.toString());
+                log.info("Konfiguration erfolgreich gespeichert!");
+            } catch (IOException e) {
+                Popups.createPopup(Alert.AlertType.ERROR, e.getMessage(), "Konnte nicht auf Datei zugreifen");
+            }
+        } else {
+            log.info("Eingaben fehlerhaft -> konnte nicht gespeichert werden");
+        }
+    }
+
+    private JsonObject generateJson() {
+        //TODO What to do with the sources from textarea?
+        String maxWordsString = null;
+        String minWordsString = null;
+        boolean validData = true;
+        JsonObject result;
+
+        try {
+            maxWordsString = GuiParser.parseTextField(txtFieldMaxWordCount, "Max Wörter", "\\d*");
+            minWordsString = GuiParser.parseTextField(txtFieldMinWordCount, "Min. Wörter", "\\d*");
+        } catch (InputFieldEmptyException e) {
+            Popups.createPopup(Alert.AlertType.ERROR, e.getMessage(), "Eingabe leer!");
+            log.severe(e.getMessage());
+            validData = false;
+        } catch (InvalidInputException e) {
+            Popups.createPopup(Alert.AlertType.ERROR, e.getMessage(), "Eingabe ungültig!");
+            log.severe(e.getMessage());
+            validData = false;
+        }
+
+        if (validData) {
+            //TODO let user know only first 3 chars are read or limit textfield
+            int minWords = minWordsString.length() > 3 ? Integer.parseInt(minWordsString.substring(0, 3)) : Integer.parseInt(minWordsString.substring(0, 3));
+            int maxWords = maxWordsString.length() > 3 ? Integer.parseInt(maxWordsString.substring(0, 3)) : Integer.parseInt(maxWordsString);
+            if (maxWords - minWords <= 0) {
+                Popups.createPopup(Alert.AlertType.ERROR, "Max. Wörter muss größer sein als Min. Wörter!", "Ungültige Eingabe");
+                log.severe("Max. Wörter war kleiner Min. Wörter");
+                return null;
+            }
+
+            result = Json.createObjectBuilder()
+                    .add("min", minWords)
+                    .add("max", maxWords)
+                    .add("min_distance", 50)
+                    .build();
+            log.info(result.toString());
+            return result;
+        } else {
+            return null;
+        }
+    }
 
 
 }
--- src/main/java/Gui/ControllerMainWindow.java
+++ src/main/java/Gui/ControllerMainWindow.java
@@ -4,11 +4,20 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import Utils.Popups;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 import java.io.IOException;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.logging.Logger;
 
 public class ControllerMainWindow {
@@ -25,19 +34,34 @@ public class ControllerMainWindow {
     public TextField txtFieldKeywords;
 
     public void generateArticle() {
-        log.info("Generating Article");
+        log.info(readConfig().toString());
     }
 
    public void openConfig() {
         try {
-            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("path/to/other/view.fxml"));
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("fakenews-config.fxml"));
             Stage config = new Stage();
             config.setTitle("Konfiguration");
-            config.setScene(new Scene(root, 450, 450));
+            config.setScene(new Scene(root, 600, 293.0));
+            config.setResizable(false);
             config.show();
         } catch (IOException e) {
             log.severe(e.getMessage());
         }
+   }
+
+   private JsonObject readConfig() {
+       JsonObject configData = null;
+        try {
+            String text = new String(Files.readAllBytes(Paths.get("config.json")), StandardCharsets.UTF_8);
+            JsonReader jsonReader = Json.createReader(new StringReader(text));
+            configData = jsonReader.readObject();
+            jsonReader.close();
+        } catch (IOException e) {
+            Popups.createPopup(Alert.AlertType.ERROR, "Die Datei \"config.json\" konnte nicht geöffnet werden!", "Fehlende Konfiguration");
+            log.severe(e.getMessage());
+        }
+        return configData;
    }
 
 }
