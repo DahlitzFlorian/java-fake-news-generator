f2e0b854b9f3927c54a064d0299fa5352276588d
Tim Leuschner <tim.leuschner@gmail.com>
2018-11-11 20:18:28 +0100

    Added a basic useless test

---
+++ src/main/test/java/GuiTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GuiTest {
+    //TODO write real tests
+    @Test
+    public void gui() {
+        assertEquals(1,1);
+
+    }
+}
