be3a5057d4135b6a2cb498988bb883cd1de8d310
Tim Leuschner <tim.leuschner@gmail.com>
2018-11-14 13:22:24 +0100

    deleted exceptions

--- src/main/java/Exceptions/InputFieldEmptyException.java
+++
@@ -1,7 +0,0 @@
-package Exceptions;
-
-public class InputFieldEmptyException extends Exception {
-    public InputFieldEmptyException(String fieldName) {
-        super("Das Eingabefeld " + fieldName + " muss ausgefüllt werden!");
-    }
-}
