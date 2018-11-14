1444f425d964e7b4191c3af9f6aaca1614baaee3
florian dahlitz <f2dahlitz@freenet.de>
2018-11-14 22:56:32 +0100

    feat: added basic control flow of text synthesis

--- src/main/java/TextSynthesis/TextSynthesis.java
+++ src/main/java/TextSynthesis/TextSynthesis.java
@@ -1,12 +1,35 @@
 package TextSynthesis;
 
+import Configuration.Configuration;
+import TextAllocation.TextAllocation;
+import TextClassification.TextClassification;
+
+import javax.json.JsonArray;
+import javax.json.JsonObject;
 import java.util.List;
 
 public class TextSynthesis {
 
     public String createArticle(List<String> keywords) {
-        String statusCode = "";
+        String statusCode = "Status: 200 - Successful.";
+
+        Configuration config = new Configuration();
+        JsonObject textConfig = config.getTextConfigurations();
+        List<String> sources = config.getSources(textConfig);
+
+        TextAllocation textAllocation = new TextAllocation();
+        JsonArray unanalysedTexts = textAllocation.getTexts(keywords, sources);
+
+        TextClassification textClassification = new TextClassification();
+        JsonArray analysedTexts = textClassification.getAnalysedTexts(unanalysedTexts);
+
+        String article = this.synthesise(analysedTexts);
+        this.save(article);
 
         return statusCode;
     }
+
+    private String synthesise(JsonArray analyzedTexts) { return ""; }
+
+    private void save(String article) { return; }
 }
