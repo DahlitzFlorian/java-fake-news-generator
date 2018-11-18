b57e7629f2cdb4bfce7e0f9e9eb5f988210d0b74
Marianne Huber <Marianne Huber@LAPTOP-5OTS3E6N.fritz.box>
2018-11-18 23:50:21 +0100

    added method searchImage using ajax.googleapis.com

--- src/main/java/ImageAllocation/ImageAllocation.java
+++ src/main/java/ImageAllocation/ImageAllocation.java
@@ -4,14 +4,17 @@ import org.apache.commons.io.FilenameUtils;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 /**
  * @author Huber
@@ -31,4 +34,23 @@ public class ImageAllocation {
 			}
 		}
 	}
+
+	public String searchImage(String keyword) throws IOException {
+		URL url = new URL("https://ajax.googleapis.com/ajax/services/search/images?"
+				+ "v=1.0&q=barack%20obama&userip=INSERT-USER-IP");
+		String finalWebsite;
+		URLConnection internetCon = url.openConnection();
+		internetCon.addRequestProperty(keyword, "http//google.com/"); // If we want to use google
+
+		StringBuilder stringBuilder = new StringBuilder();
+		String lineAtWebsite;
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(internetCon.getInputStream()));
+		while ((lineAtWebsite = bufferedReader.readLine()) != null) {
+			stringBuilder.append(lineAtWebsite);
+		}
+
+		return finalWebsite = stringBuilder.toString();
+
+	}
+
 }
--- src/main/java/ImageAllocation/ImageMain.java
+++ src/main/java/ImageAllocation/ImageMain.java
@@ -10,6 +10,7 @@ public class ImageMain {
 
 	public static void main(String[] args) throws IOException {
 		ImageAllocation imageAllocation = new ImageAllocation();
+		System.out.println(imageAllocation.searchImage("baum"));
 		imageAllocation.getImage(
 				"https://de.wikipedia.org/wiki/Duale_Hochschule_Baden-W%C3%BCrttemberg_Mannheim#/media/File:DHBW_MA_Logo.jpg",
 				new File("").getAbsolutePath());
