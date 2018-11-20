7afb4c87d2fc7833300aa1657adf02bbed458e72
Tim Leuschner <tim.leuschner@gmail.com>
2018-11-20 20:47:53 +0100

    added a method splitting words and counting Words

--- src/main/java/TextClassification/TextAnalyzer.java
+++ src/main/java/TextClassification/TextAnalyzer.java
@@ -1,4 +1,5 @@
 package TextClassification;
+
 import java.util.*;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -23,142 +24,179 @@ import javax.json.JsonReader;
  */
 
 class TextAnalyzer implements Analyzer {
-	JsonArray mJsonArray;
+    JsonArray mJsonArray;
 
-	public int getEmptyParagraphs(String words[]) {
-		String line;
-		int emptyLine = 0;
+    public int getEmptyParagraphs(String words[]) {
+        String line;
+        int emptyLine = 0;
 
-		try {
-			// Create a reader which reads json-files
-			InputStream fileInputStream = new FileInputStream(JSON_FILE);
-			JsonReader jsonReader = Json.createReader(fileInputStream);
-			mJsonArray = jsonReader.readArray();
+        try {
+            // Create a reader which reads json-files
+            InputStream fileInputStream = new FileInputStream(JSON_FILE);
+            JsonReader jsonReader = Json.createReader(fileInputStream);
+            mJsonArray = jsonReader.readArray();
 
-			int emptyParagraphCounter = 0;
-			int lineNumber = 0;
-			while ((line = ((BufferedReader) mJsonArray).readLine()) != null) {
-				if (line.equals("")) {
-					lineNumber++;
-					emptyParagraphCounter++;
-					System.out.println("The" + emptyParagraphCounter + "empty paragraph is in line" + lineNumber);
-					emptyLine = lineNumber;
-				} else if (!(line.equals(""))) {
-					lineNumber++;
-				}
-			}
+            int emptyParagraphCounter = 0;
+            int lineNumber = 0;
+            while ((line = ((BufferedReader) mJsonArray).readLine()) != null) {
+                if (line.equals("")) {
+                    lineNumber++;
+                    emptyParagraphCounter++;
+                    System.out.println("The" + emptyParagraphCounter + "empty paragraph is in line" + lineNumber);
+                    emptyLine = lineNumber;
+                } else if (!(line.equals(""))) {
+                    lineNumber++;
+                }
+            }
 
-			// Close jsonReader and fileInputStream
-			jsonReader.close();
-			fileInputStream.close();
+            // Close jsonReader and fileInputStream
+            jsonReader.close();
+            fileInputStream.close();
 
-		} catch (IOException e) {
-			System.out.println(e.toString());
-		}
-		return emptyLine;
-	}
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+        return emptyLine;
+    }
 
-	public int searchKeywordsAndLines(String words[]) {
-		String fileLine = "";
-		String searchedWord = words[0];
+    public int searchKeywordsAndLines(String words[]) {
+        String fileLine = "";
+        String searchedWord = words[0];
 
-		int lineNumber = 0;
-		int countWord = 0;
+        int lineNumber = 0;
+        int countWord = 0;
 
-		if (words.length > 0) {
+        if (words.length > 0) {
 
-			try {
-				// Create a reader which reads json-files
-				InputStream fileInputStream = new FileInputStream(JSON_FILE);
-				JsonReader jsonReader = Json.createReader(fileInputStream);
-				mJsonArray = jsonReader.readArray();
+            try {
+                // Create a reader which reads json-files
+                InputStream fileInputStream = new FileInputStream(JSON_FILE);
+                JsonReader jsonReader = Json.createReader(fileInputStream);
+                mJsonArray = jsonReader.readArray();
 
-				// Close jsonReader and fileInputStream
-				jsonReader.close();
-				fileInputStream.close();
+                // Close jsonReader and fileInputStream
+                jsonReader.close();
+                fileInputStream.close();
 
-				// Search position and how often this words appear in this text
-				while ((fileLine = ((BufferedReader) mJsonArray).readLine()) != null) {
-					lineNumber++;
-					int position = fileLine.indexOf(searchedWord);
+                // Search position and how often this words appear in this text
+                while ((fileLine = ((BufferedReader) mJsonArray).readLine()) != null) {
+                    lineNumber++;
+                    int position = fileLine.indexOf(searchedWord);
 
-					if (position > -1) {
-						countWord++;
-						System.out.println("The word is at " + position + ", line " + lineNumber);
-					}
-				}
+                    if (position > -1) {
+                        countWord++;
+                        System.out.println("The word is at " + position + ", line " + lineNumber);
+                    }
+                }
 
-				// Close BufferedReader
-				((BufferedReader) mJsonArray).close();
-			} catch (IOException e) {
-				System.out.println(e.toString());
-			}
-		} else {
-			System.out.println("Please enter a word.");
-		}
+                // Close BufferedReader
+                ((BufferedReader) mJsonArray).close();
+            } catch (IOException e) {
+                System.out.println(e.toString());
+            }
+        } else {
+            System.out.println("Please enter a word.");
+        }
 
-		return countWord;
-	}
-
-
-	/**
-	 * Method to count Nominals. Nominals are recognized by regex. If a string contains a Nominal at least twice, the Nominal
-	 * is counted by using a JSON.
-	 *
-	 * @author Fichte
-	 */
-
-	public class testAnalyzer {
-		public Map<String, Integer> searchNominal(JsonArray article) {
-			String line;
-			ArrayList<String> nominal = new ArrayList<String>();
-			Map<String, Integer> nominalCounter = new HashMap<String, Integer>();
-			int tempNumber;
-			String news;
-			String[] newsArray;
-			news = article.toString();
-			news = news.replaceAll("/\n/g,", " ");  //not working probably !!
-			newsArray = news.split(" ");
-			for (int i = 0; i < newsArray.length; i++) {
-				String tempword = newsArray[i].toString();
-				if (tempword.matches("^[A-Z](.*)") && nominal.contains(tempword)) {
-					tempNumber = nominalCounter.get(tempword) + 1;
-					nominalCounter.put(tempword, tempNumber);
-				} else if (tempword.matches("^[A-Z](.*)")) {
-					nominal.add(tempword);
-					nominalCounter.put(tempword, 1);
-
-				}
+        return countWord;
+    }
 
 
-			}
+    /**
+     * Method to count Nominals. Nominals are recognized by regex. If a string contains a Nominal at least twice, the Nominal
+     * is counted by using a JSON.
+     *
+     * @author Fichte
+     */
 
-			for (String name : nominal) {
-				System.out.println(name);
-			}
+    public class testAnalyzer {
+        public Map<String, Integer> searchNominal(JsonArray article) {
+            String line;
+            ArrayList<String> nominal = new ArrayList<>();
+            Map<String, Integer> nominalCounter = new HashMap<>();
+            int tempNumber;
+            String news;
+            String[] newsArray;
+            news = article.toString();
+            news = news.replaceAll("/\n/g,", " ");  //not working probably !!
+            newsArray = news.split(" ");
+            for (int i = 0; i < newsArray.length; i++) {
+                String tempword = newsArray[i].toString();
+                if (tempword.matches("^[A-Z](.*)") && nominal.contains(tempword)) {
+                    tempNumber = nominalCounter.get(tempword) + 1;
+                    nominalCounter.put(tempword, tempNumber);
+                } else if (tempword.matches("^[A-Z](.*)")) {
+                    nominal.add(tempword);
+                    nominalCounter.put(tempword, 1);
 
-			return nominalCounter;
-		}
-	}
+                }
 
 
-	/**
-	 * Method gives back the top elements with the top values. The limit is set by the ratio variable. Method filters
-	 * for the msot frequently used Nominals in the article
-	 *
-	 * @author Fichte
-	 */
+            }
 
-	public  Map<String, Integer> getMostFrequentlyNominals(Map<String, Integer> Nominals){
-		Map<String, Integer> mostFrequentlyUsed = new HashMap<>();
-		int amountOfElements = Nominals.size();
-		long ratio = Math.round(amountOfElements * 0.025);
-		mostFrequentlyUsed =
-				Nominals.entrySet().stream()
-						.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-						.limit(ratio)
-						.collect(Collectors.toMap(
-								Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
-		return mostFrequentlyUsed;
-	}
+            for (String name : nominal) {
+                System.out.println(name);
+            }
+
+            return nominalCounter;
+        }
+    }
+
+
+    /**
+     * Method gives back the top elements with the top values. The limit is set by the ratio variable. Method filters
+     * for the msot frequently used Nominals in the article
+     *
+     * @author Fichte
+     */
+
+    public Map<String, Integer> getMostFrequentlyNominals(Map<String, Integer> Nominals) {
+        Map<String, Integer> mostFrequentlyUsed = new HashMap<>();
+        int amountOfElements = Nominals.size();
+        long ratio = Math.round(amountOfElements * 0.025);
+        mostFrequentlyUsed =
+                Nominals.entrySet().stream()
+                        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                        .limit(ratio)
+                        .collect(Collectors.toMap(
+                                Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+        return mostFrequentlyUsed;
+    }
+
+
+    /**
+     * @param corpus      the text to be split
+     * @param punctuation true removes punctuation, false just splits whitespaces
+     * @return an Array containing every word of the corpus; doesn't remove duplictes
+     * @author Leuschner
+     * Method to split a corpus of text into each of its words either removing punctuation
+     * or not
+     */
+    private String[] splitWords(String corpus, boolean punctuation) {
+        if (punctuation)
+            return corpus.replaceAll("\\p{Punct}", "").split("\\s");
+        else
+            return corpus.split("\\s");
+    }
+
+    /**
+     * @param wordList an array of the words to be counted
+     * @return a Map containing the word as key and the number of occurrences as value
+     * @author Leuschner
+     * Method to count how many times a word occures in an array of words
+     * For this method to work each entry in the array must be a single word
+     */
+    private Map<String, Integer> wordOccurrence(String[] wordList) {
+        Map<String, Integer> wordOccurrence = new HashMap<>();
+        for (String word : wordList) {
+            if (!wordOccurrence.containsKey(word)) {
+                wordOccurrence.put(word, 1);
+            } else {
+                int occurence = wordOccurrence.get(word);
+                wordOccurrence.put(word, ++occurence);
+            }
+        }
+        return wordOccurrence;
+    }
+
 }
