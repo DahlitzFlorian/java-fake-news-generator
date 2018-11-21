0fdea3483d36d48bf0abe517bcb001a672aa114c
Florian Dahlitz <f2dahlitz@freenet.de>
2018-11-21 11:29:50 +0100

    feat: integrated full feed request into FeedParser -> fix #19

--- src/main/java/TextAllocation/FeedParser.java
+++ src/main/java/TextAllocation/FeedParser.java
@@ -21,6 +21,11 @@ Class for text creation. The input parameter is a keyword. The artikelBeschaffun
 class FeedParser implements FeedParserInterface {
 
     public JsonArray getTexts(String source, String[] keywords) {
+        final String fullFeed = "https://www.freefullrss.com/feed.php?url=";
+        final String fullFeedOptions = "&max=20&links=preserve&exc=&submit=Create+Full+Text+RSS";
+
+        source = fullFeed + source + fullFeedOptions;
+
         JsonArrayBuilder articles = Json.createArrayBuilder();
 
         String news;
