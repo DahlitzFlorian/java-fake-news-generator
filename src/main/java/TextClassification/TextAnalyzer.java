ee9c1160ffdcc4eb90c6cf343f636a36a093491d
Jan-Niklas FICHTE <s170657@student.dhbw-mannheim.de>
2018-12-02 19:41:38 +0100

    First try to seperate Paragraphs. Untested in Working Condition do to disconnection with freefullrss.com

--- src/main/java/TextClassification/TextAnalyzer.java
+++ src/main/java/TextClassification/TextAnalyzer.java
@@ -191,8 +191,8 @@ class TextAnalyzer implements Analyzer {
             JsonObject temp = article.getJsonObject(i);
             for (int j = 0; j < temp.size(); j++) {
                 json = article.toString();
-                json =json.replaceAll("\\{\\[","");
-                json = json.replaceAll("]}","");
+                json =json.replaceAll("\\[\\{","");
+                json = json.replaceAll("}]","");
                 String [] tempArray = json.split("\\r?\\n");
                 listOfParagraphs = new ArrayList<String>(Arrays.asList(tempArray));
             }
